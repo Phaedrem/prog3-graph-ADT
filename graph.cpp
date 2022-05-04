4fa3975a5155d625ac59734914ea49526b4878ea
Darren Bowers <darren.bowers@g.austincc.edu>
2022-05-04 18:33:52 -0500

    updated binary search and addVertex to add a vertex to an already existing vector list as well as updating constructor/destructor and print methods to test

--- graph.cpp
+++ graph.cpp
@@ -20,30 +20,32 @@ CONSTRUCTORS / DESTRUCTORS
 Graph::Graph(){
     vertexCount = 0;
     edgeCount = 0;
-    for(int i=0; i<5; i++){
+    for(int i=0; i<10; i++){
         DataVertex* newVertex = new DataVertex;
         graphList.push_back(newVertex);
-        graphList[i]->data.id = i;
+        graphList[i]->data.id = (i+1)*2;
         graphList[i]->data.information = "RANDOM";
         graphList[i]->next = nullptr;
+        vertexCount++;
     }
     DataVertex* testVertex1 = new DataVertex;
     testVertex1->data.id = 100;
     testVertex1->data.information = "MONKEY";
     testVertex1->next = nullptr;
     graphList[0]->next = testVertex1;
+    edgeCount++;
 
     DataVertex* testVertex2 = new DataVertex;
     testVertex2->data.id = 88;
     testVertex2->data.information = "SNAKE";
-    testVertex2->data.edgeWeight = 8;
     testVertex2->next = nullptr;
     graphList[0]->next->next = testVertex2;
+    edgeCount++;
 }
 
 Graph::~Graph(){
     DataVertex* current;
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < vertexCount; i++){
         current = graphList[i]->next;
         while(current){
             graphList[i]->next = current->next;
@@ -60,8 +62,19 @@ PRIVATE
 *********************************/
 
 
-int Graph::binarySearch(int id){
-    return 0;
+int Graph::binarySearch(int beginning, int end, int id){
+    int mid = -1;
+    while(beginning <= end){
+        mid = beginning+(end-beginning)/2;
+        if(graphList[mid]->data.id < id){
+            beginning = mid+1;
+        }else if(graphList[mid]->data.id > id){
+            end = mid-1;
+        }else{
+            beginning = end+1;
+        }
+    }
+    return mid;
 }
 
 
@@ -82,8 +95,25 @@ int Graph::getEdgeWeight(int startVertex, int endVertex){
     return 0;
 }
 
-bool Graph::addVertex(int id, Data* info){
-    return true;
+bool Graph::addVertex(int id, string* info){
+    bool added = false;
+    if(id > 0 && *info != ""){
+        int mid = (vertexCount-1)/2;
+        int placeHolder = binarySearch(0, vertexCount-1, id);
+        if(id != graphList[placeHolder]->data.id){
+            DataVertex* testVertex3 = new DataVertex;
+            testVertex3->data.id = id;
+            testVertex3->data.information = *info;
+            testVertex3->next = nullptr;
+            if(id > graphList[mid-1]->data.id && id < graphList[mid]->data.id){
+                placeHolder = placeHolder+1;
+            }
+            graphList.insert(graphList.begin()+placeHolder, testVertex3);
+            vertexCount++;
+            added = true;
+        }
+    }
+    return added;
 }
 
 bool Graph::addEdge(int startVertex, int endVertex, int weight){
@@ -100,7 +130,7 @@ bool Graph::removeEdge(int startVertex, int endVertex){
 
 void Graph::depthFirstTraversal(int startVertex){
     DataVertex* current; 
-    for(int i=0; i<5; i++){
+    for(int i=0; i<vertexCount; i++){
         cout << "vertex " << graphList[i]->data.id << " contains: " << graphList[i]->data.information << endl;
         current = graphList[i]->next;
         while(current){
@@ -112,7 +142,7 @@ void Graph::depthFirstTraversal(int startVertex){
 
 void Graph::breadthFirstTraversal(int startVertex){
     DataVertex* current; 
-    for(int i=0; i<5; i++){
+    for(int i=0; i<vertexCount; i++){
         cout << "vertex " << graphList[i]->data.id << " contains: " << graphList[i]->data.information << endl;
         current = graphList[i]->next;
         while(current){
