728d32775f9fa411e0c084317a1c02b935096a5f
Darren Bowers <darren.bowers@g.austincc.edu>
2022-05-04 18:32:11 -0500

    added calls for addVertex and print to test

--- main.cpp
+++ main.cpp
@@ -9,9 +9,14 @@ Purpose: Creating a graph ADT
 int main(int argc, char** argv){
     srand(time(NULL));
 
+    string stringName = "TURTLE";
+
     Graph newGraph;
     newGraph.breadthFirstTraversal(9);
+    cout << endl << endl; 
+    newGraph.addVertex(221, &stringName);
 
+    newGraph.depthFirstTraversal(9);
 
     return 0;
 }
