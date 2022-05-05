fe366c6cda1b8752a7da3ea115293645ed63dcf7
Darren Bowers <darren.bowers@g.austincc.edu>
2022-05-04 22:52:36 -0500

    renamed struct to Vertex, replaced next with a pair vector and named it edges. Included vector and utility to accommodate

--- data.h
+++ data.h
@@ -8,16 +8,21 @@ Purpose: Creating a graph ADT
 #define GRAPH_DATA_H
 
 #include <string>
+#include <vector>
+#include <utility>
+
 using std::string;
+using std::vector;
+using std::pair;
 
 struct Data {
     int id;
     string information;
 };
 
-struct DataVertex {
+struct Vertex{
     Data data;
-    DataVertex *next;
+    vector<pair<int,int>> edges;
 };
 
 #endif /* GRAPH_DATA_H */
