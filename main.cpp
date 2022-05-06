147977e28d38b8c1ae71825865927cbcc0467ddb
Darren Bowers <darren.bowers@g.austincc.edu>
2022-05-05 19:21:44 -0500

    changed calls for addVertex and addEdges to test new depthFirstTraversal

--- main.cpp
+++ main.cpp
@@ -14,35 +14,37 @@ int main(int argc, char** argv){
 
     cout << endl;
     cout << endl << endl; 
-    for(int i = 0; i<=10; i++){
+    for(int i = 0; i<=5; i++){
         if(newGraph.addVertex(i*2, &stringName)){
             cout << "added " << i*2 << ": " << stringName << endl;
         }
     }
-    newGraph.depthFirstTraversal(9);
+    newGraph.breadthFirstTraversal(9);
     cout << endl;
 
     stringName = "HARE";
-    for(int i = 0; i<=25; i++){
+    for(int i = 0; i<=10; i++){
         if(newGraph.addVertex(i, &stringName)){
             cout << "added " << i << ": " << stringName << endl;
         }
     }
     cout << endl;
 
-    newGraph.depthFirstTraversal(9);
+    newGraph.breadthFirstTraversal(1);
 
     cout << endl << "Current vertex count is " << newGraph.getNumVertices() << " and current edge count is " << newGraph.getNumEdges() << endl;
 
     cout << endl;
 
     newGraph.addEdge(1, 4, 10);
-    newGraph.addEdge(1, 25, 88);
+    newGraph.addEdge(1, 10, 88);
+    newGraph.addEdge(2, 7, 88);
+    newGraph.addEdge(4, 9, 88);
+    newGraph.addEdge(4, 3, 88);
 
     cout << endl << "Current vertex count is " << newGraph.getNumVertices() << " and current edge count is " << newGraph.getNumEdges() << endl;
 
-    newGraph.addEdge(1, 4, 10);
-    for(int i = 1; i<=25; i++){
+    for(int i = 1; i<=10; i++){
         cout << "creating edge between " << 1 << " and " << i+1 << "... ";
         if(newGraph.addEdge(1, i+1, i*2)){
             cout << "success" << endl;
@@ -52,7 +54,7 @@ int main(int argc, char** argv){
     }
 
 
-    newGraph.depthFirstTraversal(9);
+    newGraph.depthFirstTraversal(1);
 
     cout << endl << "Current vertex count is " << newGraph.getNumVertices() << " and current edge count is " << newGraph.getNumEdges() << endl;
 
