d13ab10db9a1c6d9a581887e6753e08018b7b7ae
Darren Bowers <darren.bowers@g.austincc.edu>
2022-05-07 14:30:02 -0500

    corrected an error with createEdge that would incorrectly mark it as true. Also corrected an error with breadthFirstAssist that was causing it to stop early

--- graph.cpp
+++ graph.cpp
@@ -70,19 +70,22 @@ bool Graph::createEdge(int vertex, int weight, int index){
     if(graphList[index]->edges.front().first == -1){
         graphList[index]->edges[STARTPOSITION].first = vertex;
         graphList[index]->edges[STARTPOSITION].second = weight;
+        created = true;
     }else if(vertex > graphList[index]->edges.back().first){
         graphList[index]->edges.push_back(make_pair(vertex, weight));
+        created = true;
     }else{
         int nestedIndex = edgeBinarySearch(index, STARTPOSITION, graphList[index]->edges.size()-1, vertex);
         if(vertex != graphList[index]->edges[nestedIndex].first){
             if(vertex < graphList[index]->edges[nestedIndex].first){
                 graphList[index]->edges.insert(graphList[index]->edges.begin()+nestedIndex, make_pair(vertex,weight));
+                created = true;
             }else{
                 graphList[index]->edges.insert(graphList[index]->edges.begin()+nestedIndex+1, make_pair(vertex,weight));
+                created = true;
             }
         }
     }
-    created = true;
     return created;
 }
 
@@ -102,14 +105,17 @@ void Graph::breadthFirstAssist(int startingIndex, vector<bool> &visited, std::li
     visited[startingIndex] = true;
     cout << graphList[startingIndex]->data.id << ": " << graphList[startingIndex]->data.information << " ";
     for(int i=0; i < graphList[startingIndex]->edges.size(); i++){
+        //cout << endl <<" checking " << graphList[startingIndex]->edges[i].first << endl;
         if(!visited[vertexBinarySearch(STARTPOSITION, vertexCount-1,graphList[startingIndex]->edges[i].first)]){
             queue.push_back(graphList[startingIndex]->edges[i].first);
+            //cout << " added " << graphList[startingIndex]->edges[i].first << endl; 
         }
     }
-    if(!queue.empty()){
+    while(!queue.empty()){
         int nextIndex = vertexBinarySearch(STARTPOSITION, vertexCount-1, queue.front());
         queue.pop_front();
         if(!visited[nextIndex]){
+            //cout << endl << " going to " << graphList[nextIndex]->data.id << endl;
             breadthFirstAssist(nextIndex, visited, queue);
         }
     }
@@ -268,7 +274,7 @@ void Graph::depthFirstTraversal(int startVertex){
             }
             cout << endl;
         }else{
-            cout << "Vertex doesn't exist in graph" << endl;
+            cout << "Vertex " << startVertex << " doesn't exist in graph" << endl;
         }
     }else{
         cout << "LIST IS EMPTY" << endl;
@@ -288,7 +294,7 @@ void Graph::breadthFirstTraversal(int startVertex){
             }
             cout << endl;
         }else{
-            cout << "Vertex doesn't exist in graph" << endl;
+            cout << "Vertex " << startVertex << " doesn't exist in graph" << endl;
         }
     }else{
         cout << "LIST IS EMPTY" << endl;
