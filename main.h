2cef1c0aa870fabdf94c885aeef7da4d04440de8
Darren Bowers <darren.bowers@g.austincc.edu>
2022-05-07 12:02:19 -0500

    defined several literals for to use for testing

--- main.h
+++ main.h
@@ -13,6 +13,11 @@ Purpose: Creating a graph ADT
 #include <string>
 #include "graph.h"
 
+#define TESTDATA1 10
+#define TESTDATA2 15
+#define MIN 0
+#define MAX 100
+
 using std::cout;
 using std::endl;
 using std::string;
